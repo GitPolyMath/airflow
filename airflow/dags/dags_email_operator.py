5190615138f84620cc19505e84b8103905ee2035
GitPolyMath <wasd3030@naver.com>
2023-06-26 01:08:13 +0900

    email operator dag_id 수정 업로드

--- airflow/dags/dags_email_operator.py
+++ airflow/dags/dags_email_operator.py
@@ -4,7 +4,7 @@ import datetime
 from airflow.operators.email import EmailOperator
 
 with DAG(
-    dag_id = "dags_bash_select_fruit",
+    dag_id = "dags_email_operator",
     schedule = "0 8 * 1 1#1",
     start_date = pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup = False
