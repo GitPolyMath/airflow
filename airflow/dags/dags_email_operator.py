9b2963d3ed6d2b241d52970a2672da3b05da3a33
GitPolyMath <wasd3030@naver.com>
2023-06-26 01:14:00 +0900

    email operator task_id 수정 업로드

--- airflow/dags/dags_email_operator.py
+++ airflow/dags/dags_email_operator.py
@@ -5,14 +5,14 @@ from airflow.operators.email import EmailOperator
 
 with DAG(
     dag_id = "dags_email_operator",
-    schedule = "0 8 * 1 1#1",
+    schedule = "0 8 * * 1#1",
     start_date = pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup = False
 ) as dag:
 
     send_email_task = EmailOperator(
-        task_id = "t1_orange",
+        task_id = "send_email_task",
         to = 'matthew@wink.kr',
         subject = '(matthew Airflow작업)[Airflow 성공메일]',
-        html_content = 'Airflow 작업테스트가 완료되었습니다.',
+        html_content = 'Airflow 작업테스트가 완료되었습니다.'
     )
