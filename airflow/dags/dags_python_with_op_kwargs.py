2b86c1cfa972c5303a37504ffaca634be8c64386
GitPolyMath <wasd3030@naver.com>
2023-07-02 02:16:16 +0900

    op_kwargs test 업로드

---
+++ airflow/dags/dags_python_with_op_kwargs.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.python import PythonOperator
+from common.common_func import regist2
+
+
+with DAG(
+    dag_id = "dags_python_with_op_args" ,
+    schedule = "30 6 * * *",
+    start_date = pendulum.datetime(2023, 7, 1, tz = "Asia/Seoul"),
+    catchup = False
+) as dag:
+
+    regist_t2 = PythonOperator(
+        task_id = 'regist_2',
+        python_callable=regist2,
+        op_args = ['matthew', 'Male', 'kr' ,'seoul'],
+        op_kwargs={'email':'kakakuku@gmail.com', 'phone':'010-1234-5678'}
+    )
+
+    regist_t2
--- airflow/plugins/common/common_func.py
+++ airflow/plugins/common/common_func.py
@@ -5,3 +5,14 @@ def regist(name, sex, *args):
     print(f'이름: {name}')
     print(f'성별: {sex}')
     print(f'기타 선택사항: {args}')
+
+def regist2(name, sex, *args, **kwargs):
+    print(f'이름: {name}')
+    print(f'성별: {sex}')
+    print(f'기타 선택사항: {args}')
+    email = kwargs.get('email') or ''
+    phone = kwargs.get('phone') or ''
+    if email:
+        print(f'(선택옵션)email: {email}')
+    if phone:
+        print(f'(선택옵션)phone: {phone}')
