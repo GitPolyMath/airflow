cb96e2e55ab2bc7f65583622cefa2b377f19c4fb
GitPolyMath <wasd3030@naver.com>
2023-07-03 00:55:02 +0900

    bash macro eg1 END_DATE jinja 코드 수정 완료

--- airflow/dags/dags_bash_with_macro_eg1.py
+++ airflow/dags/dags_bash_with_macro_eg1.py
@@ -14,8 +14,8 @@ with DAG(
     # START_DATE: 전월 말일, END_DATE: 1일 전
     bash_t1 = BashOperator(
         task_id = 'bash_t1',
-        env={'START_DATE': '{{ data_interval_start.in_timezone("Asia/Seoul") | ds}}',
-             'END_DATE': '{{ (data_interval_end.in_timezone("Asia/Seoul") - macro.dateutil.relativedelta.relativedelta(days=-1)) | ds}}'
+        env={'START_DATE': '{{ data_interval_start.in_timezone("Asia/Seoul") | ds }}',
+             'END_DATE': '{{ (data_interval_end.in_timezone("Asia/Seoul") - macro.dateutil.relativedelta.relativedelta(days=1)) | ds}}'
         },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
     )
