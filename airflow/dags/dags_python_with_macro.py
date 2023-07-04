516d730b080b28846198ef72ede89fc9b3fe29b5
GitPolyMath <wasd3030@naver.com>
2023-07-05 00:33:15 +0900

    python macro relativedelta 파라미터 month(s) | day(s) s 추가 수정

--- airflow/dags/dags_python_with_macro.py
+++ airflow/dags/dags_python_with_macro.py
@@ -12,8 +12,8 @@ with DAG(
     catchup = False
 ) as dag:
     @task(task_id = 'task_using_macros',
-          templates_dict = {'start_date': '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(month=-1, day=1)) | ds }}',
-                            'end_date': '{{ (data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + macros.dateutil.relativedelta.relativedelta(day=-1)) | ds }}'
+          templates_dict = {'start_date': '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(months=-1, day=1)) | ds }}',
+                            'end_date': '{{ (data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1)) | ds }}'
         }
     )
     def get_datetime_macro(**kwargs):
@@ -31,7 +31,7 @@ with DAG(
 
         data_interval_end = kwargs['data_interval_end']
         prev_month_day_first = data_interval_end.in_timezone('Asia/Seoul') + relativedelta(momths=-1, day=1)
-        prev_month_day_last = data_interval_end.in_timezone('Asia/Seoul').replace(day=1) - relativedelta(day=-1)
+        prev_month_day_last = data_interval_end.in_timezone('Asia/Seoul').replace(day=1) - relativedelta(days=-1)
 
         print(prev_month_day_first)
         print(prev_month_day_last)
