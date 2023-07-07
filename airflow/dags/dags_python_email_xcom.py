81e5a69b8aa1bfdcfd0f81d481861644b086b9ed
GitPolyMath <wasd3030@naver.com>
2023-07-08 00:59:10 +0900

    python_email_xcom 오타 수정 완료

--- airflow/dags/dags_python_email_xcom.py
+++ airflow/dags/dags_python_email_xcom.py
@@ -12,7 +12,7 @@ with DAG(
     catchup = False
 ) as dag:
     
-    @task(task_id = 'somthing_task')
+    @task(task_id = 'something_task')
     def some_logic(**kwargs):
         from random import choice
         return choice(['Success', 'Fail'])
@@ -22,6 +22,6 @@ with DAG(
         to = 'matthew@wink.kr',
         subject = '{{ data_interval_end.in_timezone("Asia/Seoul") | ds }} some_logic 처리결과',
         html_content='{{ data_interval_end.in_timezone("Asia/Seoul") | ds }} 처리 결과는 <br> \
-                    {{ti.xcom_pull(task_ids = "something_task")}} 했습니다 <br>'
+                    {{ ti.xcom_pull(task_ids = "something_task") }} 했습니다 <br>'
     )
     some_logic() >> send_email
